d8d7cc02baf9fa14eea965601c2ae131e241cf37
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-12-16 14:24:05 -0300

    Adjust to avoid snake to leave the game area

--- snake-game.js
+++ snake-game.js
@@ -27,6 +27,16 @@ createSnake();
 let direction = "right";
 
 function startGame() {
+  if (snake[0].x > (15 * box) && direction == "right") {
+    snake[0].x = 0;
+  } else if (snake[0].x < 0 && direction == "left") {
+    snake[0].x = (16 * box);
+  } else if (snake[0].y > (15 * box) && direction == "down") {
+    snake[0].y = 0;
+  } else if (snake[0].y < 0 && direction == "up") {
+    snake[0].y == (16 * box);
+  }
+
   createBackground();
   createSnake();
 
@@ -59,17 +69,17 @@ function startGame() {
   snake.unshift(newSnakeHead);
 }
 
-  document.addEventListener("keydown", update);
-  function update(event) {
-    if (event.keyCode === 37 && direction !== "right") {
-      direction = "left";
-    } else if (event.keyCode === 38 && direction !== "down") {
-      direction = "up";
-    } else if (event.keyCode === 39 && direction !== "left") {
-      direction = "right";
-    } else if (event.keyCode === 40 && direction !== "up") {
-      direction = "down";
-    } 
+document.addEventListener("keydown", update);
+function update(event) {
+  if (event.keyCode === 37 && direction !== "right") {
+    direction = "left";
+  } else if (event.keyCode === 38 && direction !== "down") {
+    direction = "up";
+  } else if (event.keyCode === 39 && direction !== "left") {
+    direction = "right";
+  } else if (event.keyCode === 40 && direction !== "up") {
+    direction = "down";
   }
+}
 
 let game = setInterval(startGame, 200);
